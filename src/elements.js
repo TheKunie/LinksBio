f04b175c328e258d8ecd58e68c59a3561566f300
TheKunie <nofake.2links@gmail.com>
2021-10-30 16:30:02 +0200

    .

--- src/elements.js
+++
@@ -1,9 +0,0 @@
-function makeElement(id, terminal) {
-  const element = {
-    id: id,
-    terminal: terminal
-  };
-}
-
-const carbonTerm = makeElement("carbon", true);
-const carbon = makeElement("carbon", false);
